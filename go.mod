7bb6646c6da98b28667e2491e64fbc0f5ffc15e5
L. Jiang <l.jiang.1024@gmail.com>
2021-03-17 09:09:31 +0800

    feat: tuna-dumper 参数修改
    Log: `tuna-dumper   -url <URL>    -a <leave_tag>    -x <remove_tag>    -out <output>    [--append]`

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	github.com/antchfx/htmlquery v1.2.3
-	github.com/ismdeep/ismdeep-go-utils v0.0.2 // indirect
+	github.com/ismdeep/ismdeep-go-utils v0.0.2
 	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd
 )
--- tuna-dumper/main.go
+++ tuna-dumper/main.go
@@ -1,19 +1,23 @@
 package main
 
 /**
- * Usage: tuna-dumper   <URL>    <RepoName> [--append]
+ * Usage: tuna-dumper   -url <URL>   -a <leave_tag> -x <remove_tag> -out <output> [--append]
  *
  * Author: L. JIANG <l.jiang.1024@gmail.com>
  *
  */
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"github.com/antchfx/htmlquery"
 	"github.com/ismdeep/ismdeep-go-utils/args_util"
 	"golang.org/x/net/html"
+	"log"
 	"os"
 	"sort"
+	"strings"
 	"sync"
 )
 
@@ -58,7 +62,67 @@ func getSubUrls(__url__ string) (urls []string) {
 }
 
 func ShowHelpMsg() {
-	fmt.Println("Usage: tuna-dumper   -url <URL>    -out <output>    [--append]")
+	fmt.Println("Usage: tuna-dumper   -url <URL>    -a <leave_tag>    -x <remove_tag>    -out <output>    [--append]")
+}
+
+func parseOutputPath() (string, error) {
+	for i := 1; i < len(os.Args)-1; i++ {
+		if os.Args[i] == "-out" {
+			return os.Args[i+1], nil
+		}
+	}
+	return "", errors.New("[ERROR] --out argument is required")
+}
+
+func parseFilterList() []string {
+	filterList := make([]string, 0)
+	for i := 1; i < len(os.Args)-1; i++ {
+		if os.Args[i] == "-a" {
+			filterList = append(filterList, os.Args[i+1])
+		}
+	}
+	return filterList
+}
+
+func parseFilterRemoveList() []string {
+	filterRemoveList := make([]string, 0)
+	for i := 1; i < len(os.Args)-1; i++ {
+		if os.Args[i] == "-x" {
+			filterRemoveList = append(filterRemoveList, os.Args[i+1])
+		}
+	}
+	return filterRemoveList
+}
+
+func parseAppendFlag() bool {
+	for i := 1; i < len(os.Args); i++ {
+		if os.Args[i] == "--append" {
+			return true
+		}
+	}
+	return false
+}
+
+func parseUrl() (string, error) {
+	if args_util.Exists("-url") {
+		return args_util.GetValue("-url"), nil
+	}
+	return "", errors.New("[ERROR] -url not found")
+}
+
+func filter(url string, filterList []string, filterRemoveList []string) bool {
+	for _, filterTag := range filterList {
+		if !strings.Contains(url, filterTag) {
+			return false
+		}
+	}
+
+	for _, filterRemoveTag := range filterRemoveList {
+		if strings.Contains(url, filterRemoveTag) {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
@@ -67,21 +131,57 @@ func main() {
 		return
 	}
 
-	if !args_util.Exists("-url") || !args_util.Exists("-out") {
+	repoUrl, err := parseUrl()
+	if err != nil {
 		ShowHelpMsg()
-		os.Exit(1)
+		log.Fatalln(err)
+		return
 	}
 
-	repoUrl := args_util.GetValue("-url")
-	appendFlag := args_util.Exists("--append")
+	appendFlag := parseAppendFlag()
+	outputPath, err := parseOutputPath()
+	if err != nil {
+		ShowHelpMsg()
+		log.Fatalln(err)
+		return
+	}
 
-	fmt.Printf("URL: [%s]\n", repoUrl)
-	fmt.Printf("Append: %v\n", appendFlag)
+	filterList := parseFilterList()
+	filterRemoveList := parseFilterRemoveList()
+
+	log.Printf("URL: [%s]\n", repoUrl)
+	log.Printf("Append: %v\n", appendFlag)
+	log.Printf("Out: %v\n", outputPath)
+	log.Printf("Filter List: %v\n", filterList)
+	log.Printf("Filter Remove List: %v\n", filterRemoveList)
 
 	urls := getSubUrls(repoUrl)
 	sort.Strings(urls)
 
-	for _, url := range urls {
-		fmt.Println(url)
+	var f *os.File
+	if appendFlag {
+		f, err = os.OpenFile(outputPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			log.Fatalf("[ERROR]: can not open file.\n")
+			return
+		}
+	} else {
+		f, err = os.OpenFile(outputPath, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatalf("[ERROR]: can not open file.\n")
+			return
+		}
 	}
+
+	w := bufio.NewWriter(f)
+
+	for _, url := range urls {
+		if filter(url, filterList, filterRemoveList) {
+			log.Println(url)
+			w.WriteString(fmt.Sprintf("%v\n", url))
+		}
+	}
+
+	w.Flush()
+	f.Close()
 }
