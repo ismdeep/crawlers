bfb2f59e375078a27238e6714a32be17b43bf641
L. Jiang <l.jiang.1024@gmail.com>
2021-03-18 17:17:59 +0800

    update build.all.sh

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 go build -o bin/ github.com/ismdeep/crawlers/tuna-dumper
 go build -o bin/ github.com/ismdeep/crawlers/python-list
+go build -o bin/ github.com/ismdeep/crawlers/magnet-search
